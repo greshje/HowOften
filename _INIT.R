2a34828a9e9767fd31c891191d80c8ad0556f8d7
NACHC-CAD <greshje.nachc@gmail.com>
2023-10-31 07:08:26 -0400

    work in progress on init

--- _INIT.R
+++ _INIT.R
@@ -1,47 +1,22 @@
-# ---
-#
-# _INIT.R
-# 
-# ---
+# run the functionsForInit script
+source("functionsForInit.R")
 
 # ---
 #
-# install functions
+# Function to get database token and url. 
+# https://ohdsi.github.io/DatabaseOnSpark/developer-how-tos_gen_dev_keyring.html
+#
+# The createDatabaseKeyRing will only create a new key ring if it does not exist. 
+# If the keyring allready exists, you will be asked for the password used when it was created.  
+# In this example: <a_new_password_for_the_keyring_that_is_not_your_database_password>
 #
 # ---
 
-installFromCran <- function(pkgName, pkgVersion) {
-  if (!requireNamespace(pkgName, quietly = TRUE) || packageVersion(pkgName) != pkgVersion) {
-    remotes::install_version(pkgName, version = pkgVersion)
-  }
-}
-
-installFromGithub <- function(pkgName, pkgVersion) {
-  if (!requireNamespace(pkgName, quietly = TRUE) || packageVersion(pkgName) != pkgVersion) {
-    remotes::install_github(pkgName, ref=pkgVersion)
-  }
-}
-
-# ---
-#
-# installs
-#
-# ---
-
-# installs from cran
-installFromCran("remotes", "2.4.2.1")
-installFromCran("keyring", "1.3.1")
-installFromCran("DatabaseConnector", "6.2.4")
-
-#installs from github
-installFromGithub("OHDSI/Strategus", "v0.1.0")
-installFromGithub("OHDSI/CohortGenerator", "v0.8.1")
-
-# ---
-#
-# functions to get databricks token (user will be prompted for keyring password)
-#
-# ---
+createDatabaseKeyRing (
+  "databricks_keyring", 
+  "production", 
+  "<a_new_password_for_the_keyring_that_is_not_your_database_password>"
+)
 
 getToken <- function () {
   return (
@@ -53,12 +28,6 @@ getToken <- function () {
   )
 }
 
-# ---
-#
-# functions to get databricks url with token included
-#
-# ---
-
 getUrl <- function () {
   url <- "jdbc:databricks://nachc-databricks.cloud.databricks.com:443/default;transportMode=http;ssl=1;httpPath=sql/protocolv1/o/3956472157536757/0123-223459-leafy532;AuthMech=3;UseNativeQuery=1;UID=token;PWD="
   return (
@@ -66,15 +35,12 @@ getUrl <- function () {
   )  
 }
 
-# --- R Version ---------------------
-R.Version()
-# --- Java Version ------------------
-system("java -version")
-# --- Working Directory -------------
-getwd()
 
+
+# set the temp emulation schema
 options(sqlRenderTempEmulationSchema = "how_often_temp")
 
+# create your connection details
 connectionDetails <- DatabaseConnector::createConnectionDetails (
   dbms = "spark",
   connectionString = getUrl(),
@@ -82,39 +48,7 @@ connectionDetails <- DatabaseConnector::createConnectionDetails (
 )
 connectionDetailsReference <- "covid_ohdsi_connection_details"
 
-if (Sys.getenv("STRATEGUS_KEYRING_PASSWORD") == "") {
-  # set keyring password by adding STRATEGUS_KEYRING_PASSWORD='sos' to renviron
-  usethis::edit_r_environ()
-  # then add STRATEGUS_KEYRING_PASSWORD='yourPassword', save and close
-  # Restart your R Session to confirm it worked
-  stop("Please add STRATEGUS_KEYRING_PASSWORD='yourPassword' to your .Renviron file
-       via usethis::edit_r_environ() as instructed, save and then restart R session")
-}
-
-if (Sys.getenv("INSTANTIATED_MODULES_FOLDER") == "") {
-  # set a env var to a path to cache Strategus modules
-  usethis::edit_r_environ()
-  # then add INSTANTIATED_MODULES_FOLDER='path/to/module/cache', save and close
-  # Restart your R Session to confirm it worked
-  stop("Please add INSTANTIATED_MODULES_FOLDER='{path to module cache folder}' to your .Renviron file
-       via usethis::edit_r_environ() as instructed, save and then restart R session")
-}
-
-# Create the keyring if it does not exist.
-keyringName <- "HowOften"
-allKeyrings <- keyring::keyring_list()
-if (!(keyringName %in% allKeyrings$keyring)) {
-  keyring::keyring_create(keyring = keyringName, password = Sys.getenv("STRATEGUS_KEYRING_PASSWORD"))
-} else {
-  print("Keyring already exists. You do not need to create it again.")
-}
-
-# excecute this for each connectionDetails/ConnectionDetailsReference you are going to use
-Strategus::storeConnectionDetails(
-  connectionDetails = connectionDetails,
-  connectionDetailsReference = connectionDetailsReference,
-  keyringName = keyringName
-)
+storeKeyRing()
 
 # --- start test of connectionDetails -----------------------------------------------------------
 testConnection <- DatabaseConnector::connect(connectionDetails)
--- functionsForInit.R
+++ functionsForInit.R
@@ -6,6 +6,16 @@
 
 # ---
 #
+# versions
+#
+# ---
+
+R.Version()
+system("java -version")
+getwd()
+
+# ---
+#
 # install functions
 #
 # ---
@@ -20,6 +30,12 @@ installFromGithub <- function(pkgName, pkgVersion) {
   if (!requireNamespace(pkgName, quietly = TRUE) || packageVersion(pkgName) != pkgVersion) {
     remotes::install_github(pkgName, ref=pkgVersion)
   }
+}
+
+checkPackageVersion <- function(packageName) {
+  available_packages <- available.packages()
+  latest_keyring_version <- available_packages[packageName, "Version"]
+  print(latest_keyring_version)  
 }
 
 # ---
@@ -37,39 +53,76 @@ installFromCran("DatabaseConnector", "6.2.4")
 installFromGithub("OHDSI/Strategus", "v0.1.0")
 installFromGithub("OHDSI/CohortGenerator", "v0.8.1")
 
+library(remotes)
+library(keyring)
+library(DatabaseConnector)
+library(Strategus)
+library(CohortGenerator)
+
 # ---
 #
-# functions to get databricks token (user will be prompted for keyring password)
+# stratagus keyring stuff
+#
+# Basically, just use usethis::edit_r_environ() to check your .Renviron file.  
+# It should include the following
+#
+# STRATEGUS_KEYRING_PASSWORD='sos'
+# GITHUB_PAT='ghp_ThisIsMyGithubPersonalAccessTokenM2bgp91'
+# INSTANTIATED_MODULES_FOLDER='C:/_YES/_STRATEGUS/HowOften/Modules'
 #
 # ---
 
-getToken <- function () {
-  return (
-    keyring::backend_file$new()$get(
-      service = "production",
-      user = "token",
-      keyring = "databricks_keyring"
-    )
+storeKeyRing <- function() {
+  if (Sys.getenv("STRATEGUS_KEYRING_PASSWORD") == "") {
+    # set keyring password by adding STRATEGUS_KEYRING_PASSWORD='sos' to renviron
+    usethis::edit_r_environ()
+    # then add STRATEGUS_KEYRING_PASSWORD='yourPassword', save and close
+    # Restart your R Session to confirm it worked
+    stop("Please add STRATEGUS_KEYRING_PASSWORD='yourPassword' to your .Renviron file
+       via usethis::edit_r_environ() as instructed, save and then restart R session")
+  }
+  
+  if (Sys.getenv("INSTANTIATED_MODULES_FOLDER") == "") {
+    # set a env var to a path to cache Strategus modules
+    usethis::edit_r_environ()
+    # then add INSTANTIATED_MODULES_FOLDER='path/to/module/cache', save and close
+    # Restart your R Session to confirm it worked
+    stop("Please add INSTANTIATED_MODULES_FOLDER='{path to module cache folder}' to your .Renviron file
+       via usethis::edit_r_environ() as instructed, save and then restart R session")
+  }
+  
+  # Create the keyring if it does not exist.
+  keyringName <- "HowOften"
+  allKeyrings <- keyring::keyring_list()
+  if (!(keyringName %in% allKeyrings$keyring)) {
+    keyring::keyring_create(keyring = keyringName, password = Sys.getenv("STRATEGUS_KEYRING_PASSWORD"))
+  } else {
+    print("Keyring already exists. You do not need to create it again.")
+  }
+  
+  # excecute this for each connectionDetails/ConnectionDetailsReference you are going to use
+  Strategus::storeConnectionDetails(
+    connectionDetails = connectionDetails,
+    connectionDetailsReference = connectionDetailsReference,
+    keyringName = keyringName
   )
+  
 }
 
-# ---
-#
-# functions to get databricks url with token included
-#
-# ---
-
-getUrl <- function () {
-  url <- "jdbc:databricks://nachc-databricks.cloud.databricks.com:443/default;transportMode=http;ssl=1;httpPath=sql/protocolv1/o/3956472157536757/0123-223459-leafy532;AuthMech=3;UseNativeQuery=1;UID=token;PWD="
-  return (
-    paste(url, getToken(), sep = "")
-  )  
+# function to create a database keyring if it doesn't exist
+createDatabaseKeyRing <- function(kr_name, kr_service, kr_username) {
+  kb <- keyring::backend_file$new()
+  # Get a list of existing keyrings
+  existing_keyrings <- kb$keyring_list()
+  # Check if the keyring already exists
+  if (!(kr_name %in% existing_keyrings$keyring)) {
+    kb$keyring_create(kr_name)
+    kb$set(kr_service, username = kr_username, keyring = kr_name)
+    kb$keyring_lock(kr_name)
+  } else {
+    print(paste("Keyring already exists for: ", kr_name))
+  }
 }
 
-# --- R Version ---------------------
-R.Version()
-# --- Java Version ------------------
-system("java -version")
-# --- Working Directory -------------
-getwd()
+
 
